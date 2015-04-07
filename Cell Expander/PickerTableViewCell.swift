ba483cccc3f213ea9a1f1454ad276fc9fa66e7cc
Christian Di Lorenzo <rcdilorenzo@me.com>
2015-04-07 13:19:27 -0400

    Initial commit

---
+++ Cell Expander/PickerTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  PickerTableViewCell.swift
+//  Cell Expander
+//
+//  Created by Christian Di Lorenzo on 4/7/15.
+//  Copyright (c) 2015 Christian Di Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+class PickerTableViewCell : UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    class var expandedHeight: CGFloat { get { return 200 } }
+    class var defaultHeight: CGFloat  { get { return 44  } }
+    
+    func checkHeight() {
+        datePicker.hidden = (frame.size.height < PickerTableViewCell.expandedHeight)
+    }
+    
+    func watchFrameChanges() {
+        addObserver(self, forKeyPath: "frame", options: .New, context: nil)
+        checkHeight()
+    }
+    
+    func ignoreFrameChanges() {
+        removeObserver(self, forKeyPath: "frame")
+    }
+    
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if keyPath == "frame" {
+            checkHeight()
+        }
+    }
+}
---
+++ Cell Expander/TableViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Cell Expander
+//
+//  Created by Christian Di Lorenzo on 4/7/15.
+//  Copyright (c) 2015 Christian Di Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+let cellID = "cell"
+
+class TableViewController: UITableViewController {
+    var selectedIndexPath : NSIndexPath?
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellID, forIndexPath: indexPath) as PickerTableViewCell
+        cell.titleLabel.text = "Test Title"
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let previousIndexPath = selectedIndexPath
+        if indexPath == selectedIndexPath {
+            selectedIndexPath = nil
+        } else {
+            selectedIndexPath = indexPath
+        }
+        
+        var indexPaths : Array<NSIndexPath> = []
+        if let previous = previousIndexPath {
+            indexPaths += [previous]
+        }
+        if let current = selectedIndexPath {
+            indexPaths += [current]
+        }
+        if indexPaths.count > 0 {
+            tableView.reloadRowsAtIndexPaths(indexPaths, withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        (cell as PickerTableViewCell).watchFrameChanges()
+    }
+    
+    override func tableView(tableView: UITableView, didEndDisplayingCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        (cell as PickerTableViewCell).ignoreFrameChanges()
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath == selectedIndexPath {
+            return PickerTableViewCell.expandedHeight
+        } else {
+            return PickerTableViewCell.defaultHeight
+        }
+    }
+
+
+}
+
